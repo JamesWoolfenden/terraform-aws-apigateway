c829a456eec6e3909d6fcf25b4f95ed482f6ae94
James Woolfenden <james.woolfenden@gmail.com>
2023-04-24 17:36:54 +0100

    fixed new resources for new checks

--- aws_api_gateway_stage.examplea.tf
+++ aws_api_gateway_stage.examplea.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_stage" "examplea" {
   deployment_id         = aws_api_gateway_deployment.stage_api.id
   rest_api_id           = aws_api_gateway_rest_api.api.id
   stage_name            = var.stage_name
-  client_certificate_id = aws_api_gateway_client_certificate.pike
+  client_certificate_id = aws_api_gateway_client_certificate.pike.id
   cache_cluster_enabled = true
   xray_tracing_enabled  = true
   access_log_settings {
--- aws_gateway_request_validator.pike.tf
+++ aws_gateway_request_validator.pike.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_request_validator" "example" {
   name                        = var.validator.name
-  rest_api_id                 = aws_api_gateway_rest_api.example.id
+  rest_api_id                 = aws_api_gateway_rest_api.api.id
   validate_request_body       = var.validator.validate_request_body
   validate_request_parameters = var.validator.validate_request_parameters
 }
