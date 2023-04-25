87a8ac3b548615d170c9f56cb30e92f11667c85c
James Woolfenden <james.woolfenden@gmail.com>
2023-04-25 10:10:06 +0100

    strict for tfsec

--- aws_api_gateway_method.options_method.tf
+++ aws_api_gateway_method.options_method.tf
@@ -3,5 +3,6 @@ resource "aws_api_gateway_method" "options_method" {
   resource_id          = aws_api_gateway_resource.messages_resource.id
   http_method          = "OPTIONS"
   authorization        = "NONE"
+  api_key_required     = true
   request_validator_id = aws_api_gateway_request_validator.example.id
 }
