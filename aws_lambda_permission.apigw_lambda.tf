6fe14f445d4335ca15ece5abb3757eb5d679d87c
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:48:23 +0100

    fix: remove deprecated stage_name from deployment, use .id not .name for aws_region

--- aws_lambda_permission.apigw_lambda.tf
+++ aws_lambda_permission.apigw_lambda.tf
@@ -4,5 +4,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.lambda_function.arn
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/*"
 }
