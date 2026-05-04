7689412ed0e6e16fa3ed767c954707c2d3e57a02
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 23:15:47 +0100

    ci: apply pre-commit auto-fixes

--- example/exampleb/module.apigateway.tf
+++ example/exampleb/module.apigateway.tf
@@ -1,5 +1,5 @@
 module "apigateway" {
-  source = "git::https://github.com/JamesWoolfenden/terraform-aws-apigateway.git?ref=75c109a7162890715d7f60e0ca66b4b4ee5552bc" #v0.1.65
+  source          = "git::https://github.com/JamesWoolfenden/terraform-aws-apigateway.git?ref=75c109a7162890715d7f60e0ca66b4b4ee5552bc" #v0.1.65
   allowed_range   = var.allowed_range
   common_tags     = var.common_tags
   lambda_function = aws_lambda_function.examplea
